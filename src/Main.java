3324d0e6af1df64bb140156df44f1b851be02183
Leonardo Azzola <uleowz@gmail.com>
2023-07-04 02:20:24 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import model.entities.Department;
+import model.entities.Seller;
 
 import javax.xml.soap.Detail;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "nome qualquer");
         System.out.println(obj);
+
+        Seller seller = new Seller(1, "fulano", "fulano@gmail.com", new Date(), 2000.0, obj);
+        System.out.println(seller);
+        
     }
 }
