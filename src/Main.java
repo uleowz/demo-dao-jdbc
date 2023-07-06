670aa549c43fed2ea3fe9009b3739034ba752907
Leonardo Azzola <uleowz@gmail.com>
2023-07-06 04:15:53 -0300

    findByDepartment implementation

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import model.entities.Seller;
 
 import javax.xml.soap.Detail;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +15,12 @@ public class Main {
         System.out.println("TESTE Seller FindById: ");
         Seller sellerFindOutById = sellerDao.findById(3);
         System.out.println(sellerFindOutById);
-
+        System.out.println();
+        System.out.println("TESTE Seller FindByDepartment");
+        Department dep = new Department(2, null);
+        List<Seller> sellerFindOutByDepartment = sellerDao.findByDepartment(dep);
+        for (Seller obj : sellerFindOutByDepartment) {
+            System.out.println(obj);
+        }
     }
 }
